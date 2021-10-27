a2e76870c7f81d02e2a8451c17fe13cdc0b4b8b5
sandesh <sandesh>
2021-10-27 18:37:54 +0545

    first

---
+++ task1.js
@@ -0,0 +1,26 @@
+var num = [17, 19, 21]
+var sum =0;
+for(i=0;i<=num.length;i++)
+{
+  //0 conditon
+  if(num[i]===0)
+  {
+    sum+=1;
+  }
+  //case Even
+  if(num[i]%2===0)
+  {
+    sum+=1;
+  }
+  //case Even
+  if(num[i]===5)
+    {
+      sum+=5;
+    }
+    else if(num[i]%2>=1)
+    {
+      sum+=3;
+    }
+  
+}
+console.log(sum);
